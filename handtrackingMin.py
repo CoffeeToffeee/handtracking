16ce51798d7c674414bd04b9a20fc29b028dfa7a
reyansh <reyansh5111@ggn.hxls.org>
2023-10-21 14:48:15 +0530

    changed 1 line

--- handtrackingMin.py
+++ handtrackingMin.py
@@ -7,19 +7,35 @@ cap = cv2.VideoCapture(0)
 
 mpHands = mp.solutions.hands
 hands = mpHands.Hands()
-mpDraw = mp.solutions.drawing_utils # allowing use to use prebuilt draw functions
-# there aer some parameters here which you can change, im just using it do default
+mpDraw = mp.solutions.drawing_utils  # allowing use to use prebuilt draw functions
+pTime = 0
+cTime = 0
 
 while True:
     success, img = cap.read()
-    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)  # mediapipe uses rgb
+# mediapipe uses rgb cameras usually give BGR
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     results = hands.process(imgRGB)
-#   print(results.multi_hand_landmarks) checking coordinates
 
+
+# drawing the dots and lines on the hands
     if results.multi_hand_landmarks:
         for handLMS in results.multi_hand_landmarks:
-            mpDraw.draw_landmarks(img, handLMS)
-            # drawing the dots on the hands
+            for id, lm in enumerate(handLMS.landmark):
+                h, w, c = img.shape
+                cx, cy = int(lm.x*w), int(lm.y*h)
+
+
+
+
+            mpDraw.draw_landmarks(img, handLMS, mpHands.HAND_CONNECTIONS)
+
+
+# Code to display FPS
+    cTime = time.time()
+    fps = 1/(cTime-pTime)
+    pTime = cTime
+    cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_SIMPLEX, 3, (255,0,255), 3)
 
     cv2.imshow("Image", img)
     cv2.waitKey(1)
