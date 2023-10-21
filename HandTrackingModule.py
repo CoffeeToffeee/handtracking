2f5c34008ee5a62d3704a64de8ef4bca099482d1
reyansh <reyansh5111@ggn.hxls.org>
2023-10-21 15:26:37 +0530

    ue

--- HandTrackingModule.py
+++ HandTrackingModule.py
@@ -2,38 +2,52 @@ import cv2
 import mediapipe as mp
 import time
 
-# Setting up the camera
-cap = cv2.VideoCapture(0)
+class handDetector():
+    def __init__(self,mode=False, maxHands=2, detectionCon=0.5, trackCon=0.5 ):
+        self.mode = mode
+        self.maxHands = maxHands
+        self.detectionCon = detectionCon
+        self.trackCon = trackCon
 
-mpHands = mp.solutions.hands
-hands = mpHands.Hands()
-mpDraw = mp.solutions.drawing_utils  # allowing use to use prebuilt draw functions
-pTime = 0
-cTime = 0
+        self.mpHands = mp.solutions.hands
+        self.hands = self.mpHands.Hands(self.mode, self.maxHands, self.detectionCon, self.trackCon)
+        self.mpDraw = mp.solutions.drawing_utils
 
-while True:
-    success, img = cap.read()
-    # mediapipe uses rgb cameras usually give BGR
-    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    results = hands.process(imgRGB)
 
-    # drawing the dots and lines on the hands
-    if results.multi_hand_landmarks:
-        for handLMS in results.multi_hand_landmarks:
-            for id, lm in enumerate(handLMS.landmark):
-                h, w, c = img.shape
-                cx, cy = int(lm.x * w), int(lm.y * h)
+    def findHands(self, img, draw=True):
+        # mediapipe uses rgb cameras usually give BGR
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        results = self.hands.process(imgRGB)
 
-            mpDraw.draw_landmarks(img, handLMS, mpHands.HAND_CONNECTIONS)
+        # drawing the dots and lines on the hands
+        if results.multi_hand_landmarks:
+            for handLMS in results.multi_hand_landmarks:
+                if draw == true:
+                    self.mpDraw.draw_landmarks(img, handLMS, self.mpHands.HAND_CONNECTIONS)
+        return img
 
-    # Code to display FPS
-    cTime = time.time()
-    fps = 1 / (cTime - pTime)
-    pTime = cTime
-    cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_SIMPLEX, 3, (255, 0, 255), 3)
+
+def main():
+    pTime = 0
+    cTime = 0
+    # Setting up the camera
+    cap = cv2.VideoCapture(0)
+    detector = handDetector()
+
+    while True:
+        success, img = cap.read()
+        img = detector.findHands(img)
+
+
+        cTime = time.time()
+        fps = 1 / (cTime - pTime)
+        pTime = cTime
+        cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_SIMPLEX, 3, (255, 0, 255), 3)
 
     cv2.imshow("Image", img)
     cv2.waitKey(1)
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+
+if __name__ == __main__:
+    main()
+
